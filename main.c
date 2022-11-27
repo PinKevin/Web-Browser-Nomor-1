f36345dc3a9e64b00fae57695019acbdb9854122
Emerio Kevin <emeriokevin2320@gmail.com>
2022-11-27 11:15:45 +0700

    Membuat aplikasi ToNextPage dan ToPrevPage

--- main.c
+++ main.c
@@ -27,74 +27,101 @@ int main() {
     PrintInfo(A);
     printf("end PrintInfo(A)");
 
-    printf("\n\n========== ListEmpty =========");
-    printf("\nApakah list A kosong?");
-    if (ListEmpty(A)) {
-        printf(" Ya");
-    } else {
-        printf(" Tidak");
-    }
+//    printf("\n\n========== ListEmpty =========");
+//    printf("\nApakah list A kosong?");
+//    if (ListEmpty(A)) {
+//        printf(" Ya");
+//    } else {
+//        printf(" Tidak");
+//    }
 
-    printf("\n\n========== NbElmt =========");
-    printf("\nJumlah elemen pada list A = %d", NbElmt(A));
+//    printf("\n\n========== NbElmt =========");
+//    printf("\nJumlah elemen pada list A = %d", NbElmt(A));
 
     printf("\n\n========== InsertFirst =========");
     InsertFirst(&A, M);
+    InsertFirst(&A, Alokasi(3));
+    InsertFirst(&A, Alokasi(2));
     printf("\nInsertFirst(&A, M)");
     printf("\nPrintInfo(A)\n");
     PrintInfo(A);
     printf("end PrintInfo(A)");
-    printf("\nJumlah elemen pada list A = %d", NbElmt(A));
 
-    printf("\n\n========== InsertLast =========");
-    InsertLast(&A, Alokasi(6));
-    InsertLast(&A, Alokasi(61));
-    InsertLast(&A, Alokasi(38));
-    InsertLast(&A, Alokasi(2));
-    printf("\nInsertLast(&A, Alokasi(6))");
-    printf("\nInsertLast(&A, Alokasi(61))");
-    printf("\nInsertLast(&A, Alokasi(38))");
-    printf("\nInsertLast(&A, Alokasi(2))");
+    printf("\n\n========== ToNextPage =========");
+    ToNextPage(&A);
+    printf("\nToNextPage(&A)");
     printf("\nPrintInfo(A)\n");
     PrintInfo(A);
     printf("end PrintInfo(A)");
 
-    printf("\n\n========== InsertAfter =========");
-    InsertAfter(&A, Alokasi(7), M);
-    InsertAfter(&A, Alokasi(745), M);
-    InsertAfter(&A, Alokasi(91), M);
-    printf("\nInsertAfter(&A, Alokasi(7), M)");
-    printf("\nInsertAfter(&A, Alokasi(745), M)");
-    printf("\nInsertAfter(&A, Alokasi(91), M)");
+    ToNextPage(&A);
+    printf("\n\nToNextPage(&A)");
     printf("\nPrintInfo(A)\n");
     PrintInfo(A);
     printf("end PrintInfo(A)");
 
-    printf("\n\n========== DelAfter =========");
-    DelAfter(&A, &U, M);
-    printf("\nDelAfter(&A, &U, M)");
+    printf("\n\n========== ToPrevPage =========");
+    ToPrevPage(&A);
+    printf("\nToPrevPage(&A)");
     printf("\nPrintInfo(A)\n");
     PrintInfo(A);
     printf("end PrintInfo(A)");
-    printf("\nAlamat = %d", U);
 
-    printf("\n\n========== DelFirst =========");
-    DelFirst(&A, &U);
-    printf("\nDelFirst(&A, &U)");
+    ToPrevPage(&A);
+    printf("\n\nToPrevPage(&A)");
     printf("\nPrintInfo(A)\n");
     PrintInfo(A);
     printf("end PrintInfo(A)");
-    printf("\nAlamat = %d", U);
 
-    printf("\n\n========== DelLast =========");
-    DelLast(&A, &U);
-    printf("\nDelLast(&A, &U)");
-    printf("\nPrintInfo(A)\n");
-    PrintInfo(A);
-    printf("end PrintInfo(A)");
-    printf("\nAlamat = %d", U);
+//    printf("\n\n========== InsertLast =========");
+//    InsertLast(&A, Alokasi(6));
+//    InsertLast(&A, Alokasi(61));
+//    InsertLast(&A, Alokasi(38));
+//    InsertLast(&A, Alokasi(2));
+//    printf("\nInsertLast(&A, Alokasi(6))");
+//    printf("\nInsertLast(&A, Alokasi(61))");
+//    printf("\nInsertLast(&A, Alokasi(38))");
+//    printf("\nInsertLast(&A, Alokasi(2))");
+//    printf("\nPrintInfo(A)\n");
+//    PrintInfo(A);
+//    printf("end PrintInfo(A)");
+//
+//    printf("\n\n========== InsertAfter =========");
+//    InsertAfter(&A, Alokasi(7), M);
+//    InsertAfter(&A, Alokasi(745), M);
+//    InsertAfter(&A, Alokasi(91), M);
+//    printf("\nInsertAfter(&A, Alokasi(7), M)");
+//    printf("\nInsertAfter(&A, Alokasi(745), M)");
+//    printf("\nInsertAfter(&A, Alokasi(91), M)");
+//    printf("\nPrintInfo(A)\n");
+//    PrintInfo(A);
+//    printf("end PrintInfo(A)");
 
-    printf("\nBanyak elemen pada list A = %d", NbElmt(A));
+//    printf("\n\n========== DelAfter =========");
+//    DelAfter(&A, &U, M);
+//    printf("\nDelAfter(&A, &U, M)");
+//    printf("\nPrintInfo(A)\n");
+//    PrintInfo(A);
+//    printf("end PrintInfo(A)");
+//    printf("\nAlamat = %d", U);
+//
+//    printf("\n\n========== DelFirst =========");
+//    DelFirst(&A, &U);
+//    printf("\nDelFirst(&A, &U)");
+//    printf("\nPrintInfo(A)\n");
+//    PrintInfo(A);
+//    printf("end PrintInfo(A)");
+//    printf("\nAlamat = %d", U);
+//
+//    printf("\n\n========== DelLast =========");
+//    DelLast(&A, &U);
+//    printf("\nDelLast(&A, &U)");
+//    printf("\nPrintInfo(A)\n");
+//    PrintInfo(A);
+//    printf("end PrintInfo(A)");
+//    printf("\nAlamat = %d", U);
+
+//    printf("\nBanyak elemen pada list A = %d", NbElmt(A));
 
     printf("\n");
     return 0;
