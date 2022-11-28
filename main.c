3c3ccc6e220fb17d85dac1853d9fa99fd5b5eb97
Emerio Kevin <emeriokevin2320@gmail.com>
2022-11-28 19:57:39 +0700

    Menjadikan browser interaktif

--- main.c
+++ main.c
@@ -1,128 +1,87 @@
 /***********************************/
 /* Program   : main.c */
-/* Deskripsi : Aplikasi list double pointer */
+/* Deskripsi : Aplikasi browser */
 /* NIM/Nama  : 24060121120012/Emerio Kevin Aryaputra
                24060121140160/Arya Dheffan Shevchenko
 */
 /* Tanggal   : 27 November 2022 */
 /***********************************/
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 #include "list_double_pointer.h"
+#include "browser.h"
 
 int main() {
     // kamus utama
     List A;
-    address M, U;
-    infotype E;
+    address M, N;
+    boolean on;
+    char *web;
+    char pred;
+    int i;
+    size_t malloc_size;
 
     // algoritma
-    M = Alokasi(4);
-    printf("M = Alokasi(4)");
-    printf("\nAlamat = %d", M);
+    on = true;
+    i = 0;
+    malloc_size = 100;
 
-    printf("\n\n========== CreateList =========\n");
     CreateList(&A);
-    printf("CreateList(&A)");
-    printf("\nPrintInfo(A)\n");
-    PrintInfo(A);
-    printf("end PrintInfo(A)");
 
-//    printf("\n\n========== ListEmpty =========");
-//    printf("\nApakah list A kosong?");
-//    if (ListEmpty(A)) {
-//        printf(" Ya");
-//    } else {
-//        printf(" Tidak");
-//    }
-
-//    printf("\n\n========== NbElmt =========");
-//    printf("\nJumlah elemen pada list A = %d", NbElmt(A));
-
-    printf("\n\n========== InsertFirst =========");
-    InsertFirst(&A, M);
-    InsertFirst(&A, Alokasi(3));
-    InsertFirst(&A, Alokasi(2));
-    printf("\nInsertFirst(&A, M)");
-    printf("\nPrintInfo(A)\n");
-    PrintInfo(A);
-    printf("end PrintInfo(A)");
-
-    printf("\n\n========== ToNextPage =========");
-    ToNextPage(&A);
-    printf("\nToNextPage(&A)");
-    printf("\nPrintInfo(A)\n");
-    PrintInfo(A);
-    printf("end PrintInfo(A)");
-
-    ToNextPage(&A);
-    printf("\n\nToNextPage(&A)");
-    printf("\nPrintInfo(A)\n");
-    PrintInfo(A);
-    printf("end PrintInfo(A)");
-
-    printf("\n\n========== ToPrevPage =========");
-    ToPrevPage(&A);
-    printf("\nToPrevPage(&A)");
-    printf("\nPrintInfo(A)\n");
-    PrintInfo(A);
-    printf("end PrintInfo(A)");
-
-    ToPrevPage(&A);
-    printf("\n\nToPrevPage(&A)");
-    printf("\nPrintInfo(A)\n");
-    PrintInfo(A);
-    printf("end PrintInfo(A)");
-
-//    printf("\n\n========== InsertLast =========");
-//    InsertLast(&A, Alokasi(6));
-//    InsertLast(&A, Alokasi(61));
-//    InsertLast(&A, Alokasi(38));
-//    InsertLast(&A, Alokasi(2));
-//    printf("\nInsertLast(&A, Alokasi(6))");
-//    printf("\nInsertLast(&A, Alokasi(61))");
-//    printf("\nInsertLast(&A, Alokasi(38))");
-//    printf("\nInsertLast(&A, Alokasi(2))");
-//    printf("\nPrintInfo(A)\n");
-//    PrintInfo(A);
-//    printf("end PrintInfo(A)");
-//
-//    printf("\n\n========== InsertAfter =========");
-//    InsertAfter(&A, Alokasi(7), M);
-//    InsertAfter(&A, Alokasi(745), M);
-//    InsertAfter(&A, Alokasi(91), M);
-//    printf("\nInsertAfter(&A, Alokasi(7), M)");
-//    printf("\nInsertAfter(&A, Alokasi(745), M)");
-//    printf("\nInsertAfter(&A, Alokasi(91), M)");
-//    printf("\nPrintInfo(A)\n");
-//    PrintInfo(A);
-//    printf("end PrintInfo(A)");
-
-//    printf("\n\n========== DelAfter =========");
-//    DelAfter(&A, &U, M);
-//    printf("\nDelAfter(&A, &U, M)");
-//    printf("\nPrintInfo(A)\n");
-//    PrintInfo(A);
-//    printf("end PrintInfo(A)");
-//    printf("\nAlamat = %d", U);
-//
-//    printf("\n\n========== DelFirst =========");
-//    DelFirst(&A, &U);
-//    printf("\nDelFirst(&A, &U)");
-//    printf("\nPrintInfo(A)\n");
-//    PrintInfo(A);
-//    printf("end PrintInfo(A)");
-//    printf("\nAlamat = %d", U);
-//
-//    printf("\n\n========== DelLast =========");
-//    DelLast(&A, &U);
-//    printf("\nDelLast(&A, &U)");
-//    printf("\nPrintInfo(A)\n");
-//    PrintInfo(A);
-//    printf("end PrintInfo(A)");
-//    printf("\nAlamat = %d", U);
-
-//    printf("\nBanyak elemen pada list A = %d", NbElmt(A));
+    while (on){
+        system("cls");
+        MenuPage();
+        PrintInfo(A);
+        printf("\nMasukkan perintah: ");
+        scanf(" %c", &pred);
+        switch (pred){
+            case 'a':
+                web = malloc(malloc_size*sizeof(char));
+                printf("Masukkan nama situs: ");
+                scanf("%s", web);
+                M = Alokasi(web);
+                InsertFirst(&A, M);
+                break;
+            case 'b':
+                web = malloc(malloc_size*sizeof(char));
+                printf("Masukkan nama situs: ");
+                scanf("%s", web);
+                M = Alokasi(web);
+                InsertLast(&A, M);
+                break;
+            case 'd':
+                DelFirst(&A);
+                break;
+            case 'e':
+                DelLast(&A);
+                break;
+            case 'f':
+                if (NbElmt(A) > 1) {
+                    DelAfter(&A);
+                } else {
+                    printf("Hanya dapat digunakan jika terdapat dua tab atau lebih");
+                    sleep(2);
+                }
+                break;
+            case 'g':
+                ToNextPage(&A);
+                break;
+            case 'h':
+                ToPrevPage(&A);
+                break;
+            case 'l':
+                on = false;
+                printf("\nSampai jumpa lagi!");
+                break;
+            default:
+                printf("Perintah tidak dikenali\n");
+                sleep(2);
+                break;
+        }
+    }
 
     printf("\n");
     return 0;
 }
+
